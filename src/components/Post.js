c4439780fb381f2a98128184b1e2f580c2359677
sjbgit <sjb_strat@hotmail.com>
2022-10-06 00:36:03 +0000

    post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({post}) => {
   return (
-    <div>Post</div>
+    <div style={{borderStyle: 'solid', borderWidth: '2px' }}>
+    <p>
+          Content: {post.content}
+          
+            { post.image && (
+              <img src={URL.createObjectURL(post.image)} alt="selected" style={{ height: 100, width: 200, objectFit: 'cover'}} />
+            )}
+            user: {post.user}
+    </p>
+    </div>
   )
 }
 
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import Post from './Post'
 
 const PostList = ({posts}) => {
   return (
     <>
          { posts.map((post, index) => (
-          <React.Fragment key={index}><p>
+          <React.Fragment key={index}>
+            <Post post={post}/>
+          {/* <p>
           {post.content}
           
             { post.image && (
               <img src={URL.createObjectURL(post.image)} alt="selected" style={{ height: 100, width: 200, objectFit: 'cover'}} />
             )}
             user: {post.user}
-          </p>
+          </p> */}
           </React.Fragment>
         ))}
     </>    
